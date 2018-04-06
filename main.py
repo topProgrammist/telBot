57afd8f7c8b0ee1b74b6355cccd640e3304aa99d
topProgrammist <lesha.skachcov98@gmail.com>
2018-04-06 19:40:55 +0300

    test

--- main.py
+++ main.py
@@ -415,7 +415,7 @@ if 'HEROKU' in list(os.environ.keys()):
     @server.route('/')
     def webhook():
         bot.remove_webhook()
-        bot.set_webhook(url='https://brand-bot.herokuapp.com/' + TOKEN)
+        bot.set_webhook(url='https://tgbot228.herokuapp.com/' + TOKEN)
         return '!', 200
 
 
