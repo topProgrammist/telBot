4b80717b88609a5f26666f2f733c0b9b988258f6
topProgrammist <lesha.skachcov98@gmail.com>
2018-04-15 16:16:11 +0300

    test

--- main.py
+++ main.py
@@ -61,8 +61,7 @@ def greeting(message: types.Message):
     url_button = types.InlineKeyboardButton(text="Подписаться на топ-канал", url="https://t.me/brandplace")
     keyboard.add(url_button)
     bot.send_message(message.from_user.id, greeting_text, reply_markup=get_greeting_markup(),
-                     parse_mode='Markdown')
-    
+                     parse_mode='Markdown', reply_markup=keyboard)
 
 
 @bot.message_handler(func=lambda message: message.text == '🔥Инструкция для публикации🔥')
