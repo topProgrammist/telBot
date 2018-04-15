511b4b98cf41bc7dc2a6501108d7b929ce4cb2bb
topProgrammist <lesha.skachcov98@gmail.com>
2018-04-15 16:49:43 +0300

    222

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def greeting(message: types.Message):
     keyboard.add(url_button)
     bot.send_message(message.from_user.id, greeting_text, reply_markup=get_greeting_markup(),
                      parse_mode='Markdown')
-    bot.send_message(message.from_user.id, ' ыываы',
+    bot.send_message(message.from_user.id, ' @toptovarchannel',
                      parse_mode='Markdown', reply_markup=keyboard)
 
 
